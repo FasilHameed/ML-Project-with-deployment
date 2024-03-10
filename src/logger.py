c971aad85fc8ffa64cefa5dee4bffda47282d8bd
Fasil hameed <faisalhameed763@gmail.com>
2024-03-10 12:15:05 +0530

    logger testing

--- src/logger.py
+++ src/logger.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log"
 
-logs_path = os.path.join(os.cwd(),"logs",LOG_FILE)
+logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
 
 os.makedirs(logs_path,exist_ok=True)
 
@@ -16,9 +16,13 @@ LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
-    level=logging.INFO,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
 
 )
+
+
+if __name__ == "__main__":
+    logging.info("Logging has started")
 
 
