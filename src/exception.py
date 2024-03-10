ab437f5dd06ed22f7637865a76fd1bea297cb1cd
Fasil hameed <faisalhameed763@gmail.com>
2024-03-10 13:43:55 +0530

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 def error_message_details(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -8,8 +9,6 @@ def error_message_details(error, error_detail: sys):
         file_name, line_number, str(error))
     return error_message
 
-
-
 class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
@@ -18,4 +17,9 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info(f"Error occurred: {e}")
+        raise CustomException(e, sys)
